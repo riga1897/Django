3490ccc28a5711b8bde5add196172cd3957edf23
riga1897 <riga1897@yandex.ru>
2025-09-17 11:31:53 +0300

    Update urls.py
    bug fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,8 +6,6 @@ from catalog.views import contacts, home
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("/", home, name="home"),
-    path("home/", home, name="home"),  # можно сделать index
     path("contacts/", contacts, name="contacts"),
     path("", home, name="home"),
 ]
