72b98a1dd8234d0fc401c649539f0cf631724e0a
Ri Ga <riga1897@yandex.ru>
2025-09-20 16:06:01 +0300

    lesson 23.5

--- students/models.py
+++ students/models.py
@@ -1,30 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
 class Student(models.Model):
-    first_name = models.CharField(max_length=150,verbose_name='Имя')
-    last_name = models.CharField(max_length=150,verbose_name='Фамилия', unique=True)
+    FIRST_YEAR = 'first'
+    SECOND_YEAR = 'second'
+    THIRD_YEAR = 'third'
+    FOURTH_YEAR = 'fourth'
 
-    age = models.IntegerField(help_text="Введите возраст студента")
-    is_active = models.BooleanField(default=True)
-    description = models.TextField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to="photos/", verbose_name="Фотография")
-    group = models.ForeignKey(Group, on_delete=models.DO_NOTHING, related_name="students")
-    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag)
-
-    STATUS_CHOICES = [
-        ("draft", "Draft"),
-        ("published", "Published")
+    YEAR_IN_SCHOOL_CHOICES = [
+        (FIRST_YEAR, 'Первый курс'),
+        (SECOND_YEAR, 'Второй курс'),
+        (THIRD_YEAR, 'Третий курс'),
+        (FOURTH_YEAR, 'Четвертый курс'),
     ]
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
+    year = models.CharField(
+        max_length=6,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default=FIRST_YEAR,
+        verbose_name='Курс'
+    )
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f'{self.first_name} {self.last_name}'
 
     class Meta:
-        verbose_name = "студент"
-        verbose_name_plural = "студенты"
-        ordering = ["last_name"]
-        db_table = "custom_table_name"
+        verbose_name = 'студент'
+        verbose_name_plural = 'студенты'
+        ordering = ['last_name']
--- students/views.py
+++ students/views.py
@@ -1,3 +1,36 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def show_data(request):
+    if request.method != "GET":
+        return None
+    return render(request, "app/data.html")
+
+
+def submit_data(request):
+    # Обработка данных формы
+    if request.method != "POST":
+        return None
+    return HttpResponse("Данные отправлены!")
+
+
+def show_item(request, item_id):
+    return render(request, "app/item.html", {"item_id": item_id})
+
+
+def students_list(request):
+    return None
+
+
+def about(request):
+    return render(request, "students/about.html")
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        message = request.POST.get("message")
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение: {message} получено.")
+
+    return render(request, "students/contact.html")
