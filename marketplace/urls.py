95da89897a1efe3d9df46a1b13191ea18a395a00
Ri Ga <riga1897@yandex.ru>
2025-10-03 03:51:51 +0300

    Fixing

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -7,6 +7,9 @@ app_name = MarketplaceConfig.name
 
 urlpatterns = [
     path("", views.ProductsListView.as_view(), name="products_list"),
-    path("product_detail/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"),
+    path("product/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"),
+    path("product/create/", views.ProductCreateView.as_view(), name="product_create"),
+    path("product/<int:pk>/update/", views.ProductUpdateView.as_view(), name="product_update"),
+    path("product/<int:pk>/delete/", views.ProductDeleteView.as_view(), name="product_delete"),
     path("contacts/", views.ContactsView.as_view(), name="contacts"),
 ]
