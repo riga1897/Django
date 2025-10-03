1e70abbcf98a7c29774ad28e033b35419f62a4f6
Ri Ga <riga1897@yandex.ru>
2025-10-03 12:30:04 +0300

    Quick Fix

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 from .apps import BlogConfig
+
 
 app_name = BlogConfig.name
 
--- marketplace/signals.py
+++ marketplace/signals.py
@@ -1,6 +1,5 @@
 from django.db.models.signals import post_delete, pre_save
 from django.dispatch import receiver
-
 from .models import Product
 
 
